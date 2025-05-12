cb95a701491246a9d8f513f18ed5898f560cfd05
thrishujuna <thrishujuna.p@gmail.com>
2025-05-12 23:15:25 +0530

    Add files via upload

---
+++ my portfolio/script.js
@@ -0,0 +1,4 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault(); // Stop the form from submitting
+  alert('Message sent! (Not really, but this is a demo)');
+});
